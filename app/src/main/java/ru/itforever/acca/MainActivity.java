e3a7ad909b5addaa5ae64baeab5616d2543d6c60
Tkachenko Alexey <tan@it-forever.ru>
2022-01-28 22:31:02 +0700

    add class and audio

--- app/src/main/java/ru/itforever/acca/MainActivity.java
+++ app/src/main/java/ru/itforever/acca/MainActivity.java
@@ -2,13 +2,39 @@ package ru.itforever.acca;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int SELECT_AUDIO = 2;
+    public static String selectedPath = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        openGalleryAudio();
+    }
+
+    private void openGalleryAudio() {
+
+        Intent intent = new Intent();
+        intent.setType("audio/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"Select Audio "), SELECT_AUDIO);
     }
 }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (resultCode == RESULT_OK) {
+
+            if (requestCode == SELECT_AUDIO)
+            {
+                System.out.println("SELECT_AUDIO");
+                Uri selectedImageUri = data.getData();
+                selectedPath = getPath(selectedImageUri);
+                System.out.println("SELECT_AUDIO Path : " + selectedPath);
+                new SaveImageTask().execute(selectedPath);
+            }
+
+        }
+    }
